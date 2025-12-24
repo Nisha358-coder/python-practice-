11eb1b3e7d27ef88292affb1e4b0b7edbacc7bc0
Nisha Choudhary <nishuch2005.gmail.com>
2025-12-24 22:21:05 +0530

    Add for loops.py

---
+++ class04to05.py
@@ -0,0 +1,33 @@
+# Arithmetic operation 
+#  + , - , * , / , // ,** , %
+
+# a = 12 
+# b = 6
+# print(36 % 7)
+# print (12+45//56)
+
+# #assignment opertions/shorthand opertions
+# a = 6
+
+# a += 12
+# print(a)
+# a = "hello"
+# b = "sister"
+# c = a+b
+# print(c)
+
+#mcq 
+
+# x = 10
+# x %= 3
+# print(x)
+
+# x = 10
+# x /= 2
+# print(x)
+
+obtained_marks = float(450)
+total_marks = float(500)
+percentage = (obtained_marks / total_marks)*100
+print(percentage)
+
