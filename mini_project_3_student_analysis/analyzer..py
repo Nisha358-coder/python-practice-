d0494d9db2f808e3993bf22b687ecf6e2355d5d7
Nisha Choudhary <nishuch2005.gmail.com>
2026-01-14 22:06:02 +0530

    mini project 3

---
+++ mini_project_3_student_analysis/analyzer..py
@@ -0,0 +1,19 @@
+from data import students_marks
+
+result = {}
+total = 0
+
+for marks in students_marks:
+    total += marks 
+
+    if marks >= 75:
+        category = "doing very well"
+    elif marks >= 45 :
+        category = "doing okay"
+    else:
+        category = "struggling"
+result[category] = result.get(category , 0) + 1
+average = total /len(students_marks)
+print("class average:", average)
+print("class performance summary :")
+print(result)
